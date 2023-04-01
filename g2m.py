74b8354bf6056d754aa61b2b276f862125b66759
d 3 n 7 <cglass0145@bths.edu>
2023-04-02 00:59:14 +0200

    Update g2m.py

--- g2m.py
+++ g2m.py
@@ -4,7 +4,7 @@ from midiutil.MidiFile import MIDIFile
 import openai
 
 #settings
-openaiKey = '<YOUR API KEY>'
+openaiKey = '<YOUR API KEY HERE>'
 sys1 = 'You are MusicGPT, a music creation and completion chat bot that. When a user gives you a prompt,' \
           ' you return them a song showing the notes, durations, and times that they occur. Respond only with the music.' \
           '\n\nNotation looks like this:\n(Note-duration-time in beats)\nC4-1/4-0, E4-1/4-2.5, D4-1/4-3, F4-1/4-3 etc.'
@@ -13,28 +13,13 @@ sys2 = 'You are MusicGPT, a music creation and completion chat bot that. When a 
          '\n\nNotation looks like this:\nC5-1/4, E5 1/2 etc.'
 system = sys1
 
-#other vars n functions
-melody = MIDIFile(1, deinterleave=False)
-track = 0
-channel = 0
-volume = 100
-def noteToInt(n):
-    oct = int(n[-1])
-    letter = n[:-1]
-    notes = [["C"], ["Db", "C#"], ["D"], ["Eb", "D#"], ["E"], ["F"], ["Gb", "F#"], ["G"], ["Ab", "G#"], ["A"], ["Bb", "A#"], ["B"]]
-    id = 0
-    for ix, x in enumerate(notes):
-        for y in x:
-            if letter == y:
-                id = ix
-    return id+oct*12+12
-
 #environment
 path = os.path.realpath(os.path.dirname(__file__))
 
 #parse arguments
 parser = argparse.ArgumentParser()
 parser.add_argument('-p', '--prompt', help='specify prompt to use (default: Jazz!)', default='Jazz!')
+parser.add_argument('-c', '--chat', help='send follow up messages to make revisions, continuations, etc. (type \'exit\' to quit)', action='store_true')
 parser.add_argument('-m', '--mono', help='alternative system to generate monophonic MIDI outputs', action='store_true')
 parser.add_argument('-o', '--output', help='specify output directory (default: current)', default=path)
 parser.add_argument('-a', '--auth', help='specify openai api key (edit this script file to set a default)', default=openaiKey)
@@ -42,20 +27,29 @@ args = parser.parse_args()
 if args.mono:
     system = sys2
 
-counter = 1
+#other vars n functions
+history = [{'role': 'system', 'content': system}, {'role': 'user', 'content': args.prompt}]
+def noteToInt(n):
+    oct = int(n[-1])
+    letter = n[:-1]
+    notes = [['C'], ['Db', 'C#'], ['D'], ['Eb', 'D#'], ['E'], ['F'], ['Gb', 'F#'], ['G'], ['Ab', 'G#'], ['A'], ['Bb', 'A#'], ['B']]
+    id = 0
+    for ix, x in enumerate(notes):
+        for y in x:
+            if letter == y:
+                id = ix
+    return id+oct*12+12
+
 while 1:
     #openai request
     print('[*] Making request to OpenAI API')
     openai.api_key = args.auth
     r = openai.ChatCompletion.create(
         model = 'gpt-4',
-        messages = [
-            {'role': 'system', 'content': system},
-            {'role': 'user', 'content': args.prompt}
-        ]
+        messages = history
     )
     response = r['choices'][0]['message']['content']
-
+    history.append({'role': 'assistant', 'content': response})
 
     #parse content
     print('[*] Parsing content')
@@ -73,6 +67,10 @@ while 1:
 
     #make midi
     print('[*] Generating MIDI...')
+    melody = MIDIFile(1, deinterleave=False)
+    track = 0
+    channel = 0
+    volume = 100
     time = 0
     for i in noteInfo:
         pitch = i[0]
@@ -82,9 +80,16 @@ while 1:
         melody.addNote(track, channel, pitch, time, dur, volume)
         if args.mono:
             time += dur
-    if counter>=1:
+    with open(os.path.join(args.output, 'output.mid'), 'wb') as f:
+        melody.writeFile(f)
+    print('[*] Wrote the MIDI file.')
+
+    #break loop or get next prompt
+    if args.chat:
+        prompt = input('Next prompt> ')
+        if prompt == 'exit':
+            break
+        else:
+            history.append({'role': 'user', 'content': prompt})
+    else:
         break
-    counter += 1
-with open(os.path.join(args.output, 'output.mid'), 'wb') as f:
-    melody.writeFile(f)
-print('[*] Wrote the MIDI file. Bye!')
