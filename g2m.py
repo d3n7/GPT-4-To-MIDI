9756d8c271142590b6e5b18a8071ccd3048392fd
d3nt <95656307+cbg342@users.noreply.github.com>
2023-04-01 09:26:51 +0200

    Update g2m.py

--- g2m.py
+++ g2m.py
@@ -4,35 +4,16 @@ from midiutil.MidiFile import MIDIFile
 import openai
 
 #settings
-openaiKey = '<YOUR API KEY HERE>'
+openaiKey = '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'
 system = 'You are MusicGPT, a music creation and completion chat bot that. When a user gives you a prompt,' \
          'you return them a melody showing the notes and the rhythms. Respond only with the music.' \
          '\n\nNotation looks like this:\nC5-1/4, E5 1/2 etc.'
 
-#environment
-path = os.path.realpath(os.path.dirname(__file__))
-
-#parse arguments
-parser = argparse.ArgumentParser()
-parser.add_argument("-p", "--prompt", help="specify prompt to use (default: Jazz!)", default="Jazz!")
-parser.add_argument("-o", "--output", help="specify output directory (default: current)", default=path)
-parser.add_argument("-a", "--auth", help="specify openai api key (edit this script file to set a default)", default=openaiKey)
-args = parser.parse_args()
-
-#openai api
-print("[*] Making request to OpenAI API")
-openai.api_key = args.auth
-r = openai.ChatCompletion.create(
-    model = 'gpt-4',
-    messages = [
-        {'role': 'system', 'content': system},
-        {'role': 'user', 'content': args.prompt}
-    ]
-)
-content = r['choices'][0]['message']['content']
-
-#parse content
-print("[*] Parsing content")
+#other vars n functions
+melody = MIDIFile(1, deinterleave=False)
+track = 0
+channel = 0
+volume = 100
 def noteToInt(n):
     oct = int(n[-1])
     letter = n[:-1]
@@ -43,25 +24,54 @@ def noteToInt(n):
             if letter == y:
                 id = ix
     return id+oct*12+12
-noteInfo = []
-#thanks GPT-4 for this monstrosity of regex that seems to work
-for i in re.findall(r'(?<![A-Za-z\d])([A-G](?:#|b)?\d-(?:\d+\/\d+|\d+))(?![A-Za-z\d])', content):
-    note, duration = i.split("-")
-    note = noteToInt(note)
-    duration = float(Fraction(duration))*4
-    noteInfo.append([note, duration])
 
-#make midi
-print("[*] Generating MIDI file")
-melody = MIDIFile(1)
-track = 0
-channel = 0
-time = 0
-volume = 100
-for i in noteInfo:
-    pitch = i[0]
-    dur = i[1]
-    melody.addNote(track, channel, pitch, time, dur, volume)
-    time += dur
+#environment
+path = os.path.realpath(os.path.dirname(__file__))
+
+#parse arguments
+parser = argparse.ArgumentParser()
+parser.add_argument('-p', '--prompt', help='specify prompt to use (default: Jazz!)', default='Jazz!')
+parser.add_argument('-o', '--output', help='specify output directory (default: current)', default=path)
+parser.add_argument('-a', '--auth', help='specify openai api key (edit this script file to set a default)', default=openaiKey)
+parser.add_argument('-n', '--num', help='generate n number of outputs and merge them (context unaware)', type=int, default=1)
+args = parser.parse_args()
+
+counter = 1
+while 1:
+    #openai request
+    print('[*] Making request to OpenAI API')
+    openai.api_key = args.auth
+    r = openai.ChatCompletion.create(
+        model = 'gpt-4',
+        messages = [
+            {'role': 'system', 'content': system},
+            {'role': 'user', 'content': args.prompt}
+        ]
+    )
+    response = r['choices'][0]['message']['content']
+
+
+    #parse content
+    print('[*] Parsing content')
+    noteInfo = []
+    #thanks GPT-4 for this monstrosity of regex that seems to work
+    for i in re.findall(r'(?<![A-Za-z\d])([A-G](?:#|b)?\d-(?:\d+\/\d+|\d+))(?![A-Za-z\d])', response):
+        note, duration = i.split('-')
+        note = noteToInt(note)
+        duration = float(Fraction(duration))*4
+        noteInfo.append([note, duration])
+
+    #make midi
+    print('[*] Generating MIDI...')
+    time = 0
+    for i in noteInfo:
+        pitch = i[0]
+        dur = i[1]
+        melody.addNote(track, channel, pitch, time, dur, volume)
+        time += dur
+    if counter>=args.num:
+        break
+    counter += 1
 with open(os.path.join(args.output, 'output.mid'), 'wb') as f:
     melody.writeFile(f)
+print('[*] Wrote the MIDI file. Bye!')
